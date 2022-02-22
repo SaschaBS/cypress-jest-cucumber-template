56a892e8780b354c9ec63953242cd167280ee41a
Sascha Hoppe <sascha.hoppe@msg-david.de>
2022-02-22 13:14:26 +0100

    add cypress test for pets

--- cypress/integration/pet-form/then.ts
+++ cypress/integration/pet-form/then.ts
@@ -1,14 +1,4 @@
 import {Then} from "cypress-cucumber-preprocessor/steps";
-import checkTerminalText from "../support/checkTerminalText";
-import checkElementText from "../support/checkElementText";
-
-Then(/^User sees a rocket$/, function () {
-  cy.get('#rocket').should('be.visible');
-});
-
-Then(/^terminal text is (.*)$/, checkTerminalText);
-
-Then(/^the text on "(.*)" is "(.*)"$/, checkElementText);
 
 Then(/^the pet list contains "(\d*)" items$/, function (length: number) {
   cy.get('[data-cy="pet"]').should('have.length', length)
